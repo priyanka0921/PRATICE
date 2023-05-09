389d9bb5d8487c5a2eca363d4db57c0ed18db13d
Priyanka Dash <99274970+priyanka0921@users.noreply.github.com>
2023-05-09 12:16:20 +0530

    Add files via upload

---
+++ Kpattern.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+class Kpattern
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the number of * :");
+		int n= sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if( i+j==n/2 || i-j==n/2 || j==0  ) 
+				{
+					System.out.print("*");
+				}
+					else
+				{
+					System.out.print(" ");
+				}
+			}
+		System.out.println();
+		}
+	}
+} /* Enter the number of * :
+14
+*      *
+*     *
+*    *
+*   *
+*  *
+* *
+**
+*
+**
+* *
+*  *
+*   *
+*    *
+*     *
+   if(j==n/2 || i+j==n-1 && j>=n/2 ||i==j && j>n/2)

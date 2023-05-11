eca6ff051675b0c3a79cea43c0becb82efd3a025
Priyanka Dash <99274970+priyanka0921@users.noreply.github.com>
2023-05-11 23:56:22 +0530

    Add files via upload

---
+++ digitofLastno.java
@@ -0,0 +1,15 @@
+
+
+import java.util.Scanner;
+class digitofLastno
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a Number :");
+		int n = sc.nextInt();
+		int rem= n%10 ;
+	     System.out.println("its a last number of a digit " + rem);
+	}
+}
+

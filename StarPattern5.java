0dea402b0cf89071916973a54fded81ce5b283e0
Priyanka Dash <99274970+priyanka0921@users.noreply.github.com>
2023-05-14 20:02:36 +0530

    Add files via upload

---
+++ StarPattern5.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+class StarPattern5
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the number of * :");
+		int n= sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(j==0 || j==n/2 || i==0 && j<=n/2 || i==n/2 && j<=n/2 ) 
+				{
+					System.out.print("*");
+				}
+					else
+				{
+					System.out.print(" ");
+				}
+			}
+		System.out.println();
+		}
+	}
+}
+/* o/p: Enter the number of * :
+8
+*****
+*   *
+*   *
+*   *
+*****
+*   *
+*   *
+*   *

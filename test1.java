66d14fe34ffffb9807a6a2fb351d6c6e059d10f4
Priyanka Dash <99274970+priyanka0921@users.noreply.github.com>
2023-05-23 00:03:15 +0530

    Add files via upload

---
+++ test1.java
@@ -0,0 +1,21 @@
+class test 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+}
+class A
+{
+	public void m1()
+	{
+		System.out.println("this is world f java");
+	}
+}
+class B
+{
+	public void m2()
+	{
+		System.out.println(" java contained object-oriented things");
+	}
+}

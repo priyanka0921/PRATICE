53e5eaf04406e8c385384e44a446b29a53e35baa
Priyanka Dash <99274970+priyanka0921@users.noreply.github.com>
2023-08-17 23:13:13 +0530

    Add files via upload

---
+++ pattern3.java
@@ -0,0 +1,33 @@
+package pattern;
+
+public class pattern3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=5;
+		int stars=1;
+		int spaces=n-1;
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=spaces;j++)
+				System.out.print(" ");
+			for(int j=1;j<=stars;j++)
+				System.out.print("*");
+			    spaces--;
+				stars++;
+				System.out.println();
+				
+		}
+   System.out.println("--------------");
+   for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=n-i;j++)
+			System.out.print(" ");
+		for(int j=1;j<=spaces;j++)
+			System.out.print("*");
+			System.out.println();
+		}
+	}
+}
+
+
